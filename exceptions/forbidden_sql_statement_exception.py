30da711ae701865990d704a11486e56975051b5c
Kenneth KIIZA <kkiiza@SideTrade.com>
2025-11-25 15:09:51 +0000

    Refactor ForbiddenSqlStatementException.

--- exceptions/forbidden_sql_statement_exception.py
+++ exceptions/forbidden_sql_statement_exception.py
@@ -1,7 +1,9 @@
 class ForbiddenSqlStatementException(Exception):
-    def __init__(self, message: str) -> None:
-        self.message = message
+    def __init__(self, message: str):
         super().__init__(message)
 
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__} [{self.message}]"
+    def __str__(self):
+        return f"Forbidden SQL statement: {self.args[0]}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(message={self.args[0]!r})"
